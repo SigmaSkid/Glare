5d6b5ef46a827fe0632e7920cc80330386811a77
SigmaSkid <informkilioo@gmail.com>
2022-07-04 17:31:33 +0200

    multi threading and stuff

--- main.cpp
+++ main.cpp
@@ -3,20 +3,23 @@
 
 using namespace std;
 
-//#define DEBUG
-
-#ifdef DEBUG 
-using std::chrono::high_resolution_clock;
-using std::chrono::duration_cast;
-using std::chrono::duration;
-using std::chrono::milliseconds;
-#endif
-
 #define scanrange_default 12
-#define noisegate_default 25.f
+#define noisegate_default 5.f
 #define extrareactiondelay_default 0
-#define maxframeslimit_default 1024
+#define maxframeslimit_default 512
 #define triggerkey_default XK_V
+
+// evil globals
+bool is_enabled = false;
+
+void listener(KeySym triggerkey) 
+{
+    for (;;)
+    {
+        is_enabled = utils::iskeydown(triggerkey);
+        this_thread::sleep_for(std::chrono::milliseconds(16));
+    }
+}
 
 int main(int argc, char ** argv) {
 
@@ -28,7 +31,7 @@ int main(int argc, char ** argv) {
     float noisegate = noisegate_default;
     int extrareactiondelay = extrareactiondelay_default;
     int maxframeslimit = maxframeslimit_default;
-    KeySym triggerkey = triggerkey_default;
+    KeySym triggerkey = triggerkey_default; 
 
     if (argc > 1) 
     {
@@ -157,104 +160,71 @@ int main(int argc, char ** argv) {
     << " " << triggerkey
     << endl;
 
-
     color LastCol[4] = { {0,0,0},{0,0,0},{0,0,0},{0,0,0} };
     color CurCol[4] = { {0,0,0},{0,0,0},{0,0,0},{0,0,0} };
 
     bool waitTick = true;
-    bool enabled = true;
 
-    cout << "began" << endl;
+    cout << "starting listener" << endl;
+    std::thread t1(listener, triggerkey);
 
-    #ifdef DEBUG 
-    auto t1 = high_resolution_clock::now();
-    for (int i = 0; i < 50000; i++)
-    #else
+    cout << "starting main loop" << endl;
     while (true)
-    #endif 
     {
-        if (utils::iskeydown(triggerkey)) 
+        XImage * image = XGetImage(
+                    display,
+                    root,
+                    offsetx - scanrange/2,
+                    offsety - scanrange/2,
+                    scanrange,
+                    scanrange,
+                    AllPlanes,
+                    ZPixmap
+                );
+        bool cur_differences[4] = {0,0,0,0};
+
+        for (size_t i = 0; i < 4; i++)
         {
-            XImage * image = XGetImage(
-                        display,
-                        root,
-                        offsetx - scanrange/2,
-                        offsety - scanrange/2,
-                        scanrange,
-                        scanrange,
-                        AllPlanes,
-                        ZPixmap
-                    );
-
-            int cur_differences = 0;
-
-            for (size_t i = 0; i < 4; i++)
+            unsigned long pixel = XGetPixel(image, scanPoints[i].x, scanPoints[i].y);
+            unsigned char blue = pixel & image->blue_mask;
+            unsigned char green = (pixel & image->green_mask) >> 8;
+            unsigned char red = (pixel & image->red_mask) >> 16;
+    
+            CurCol[i] = {red, green, blue};
+            if (utils::compare(LastCol[i], CurCol[i], noisegate) )
             {
-                unsigned long pixel = XGetPixel(image, scanPoints[i].x, scanPoints[i].y);
-                unsigned char blue = pixel & image->blue_mask;
-                unsigned char green = (pixel & image->green_mask) >> 8;
-                unsigned char red = (pixel & image->red_mask) >> 16;
-        
-                CurCol[i] = {red, green, blue};
-
-
-                if (utils::compare(LastCol[i], CurCol[i], noisegate) )
+                if (waitTick) 
                 {
-                    if (waitTick) 
-                    {
-                        LastCol[i] = CurCol[i];
-                    }
-                    else 
-                    {
-                        cur_differences++;
-                    }
-
+                    LastCol[i] = CurCol[i];
                 }
+                cur_differences[i] = true;
             }
-
-            waitTick = false;
-
-            if (cur_differences >= 3)
+        }
+        waitTick = false;
+        if (is_enabled) 
+        {
+            // check in checkerboard pattern
+            if ((cur_differences[0] && cur_differences[3]) 
+            || (cur_differences[1] && cur_differences[2]))
             {
-                if (extrareactiondelay != 0)
-                {
-                    this_thread::sleep_for(std::chrono::milliseconds(extrareactiondelay));
-                }
-
                 // shoot
-                XTestFakeButtonEvent(display, Button1, true, 0);
+                XTestFakeButtonEvent(display, Button1, true, extrareactiondelay);
                 XFlush(display);
                 this_thread::sleep_for(std::chrono::milliseconds(1));
                 // release left mouse
                 XTestFakeButtonEvent(display, Button1, false, 0);
                 XFlush(display);
-
                 waitTick = true;
-
-                this_thread::sleep_for(std::chrono::milliseconds(sleeper*10));
             }
-
-            XDestroyImage(image);
-
         }
         else 
         {
             waitTick = true;
-            #ifndef DEBUG 
-            this_thread::sleep_for(std::chrono::milliseconds(50));
-            #endif
         }
-        // sleep, so cpu usage isn't 99999%
-        #ifndef DEBUG 
-        this_thread::sleep_for(std::chrono::milliseconds(sleeper));
-        #endif
-    }
+        XDestroyImage(image);
 
-    #ifdef DEBUG 
-    auto t2 = high_resolution_clock::now();
-    auto ms_int = duration_cast<milliseconds>(t2 - t1);
-    cout << ms_int.count() << "ms\n";
-    #endif
+        this_thread::sleep_for(std::chrono::milliseconds(sleeper));
+    }
 
     XCloseDisplay(display);
     return 0;
